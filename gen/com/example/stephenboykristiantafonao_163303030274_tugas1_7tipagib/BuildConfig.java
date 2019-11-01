3a61d6c488c2a889df217586e6302139d9d8b6ab
StephenBoy <stephenboykristian@gmaill.com>
2019-11-01 11:49:15 +0700

    pesan nya disini

---
+++ gen/com/example/stephenboykristiantafonao_163303030274_tugas1_7tipagib/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.stephenboykristiantafonao_163303030274_tugas1_7tipagib;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
